0e144eb5ddb99fe4480ffae7257354795820b16f
Joydev Barik <barikjoydev263@gmail.com>
2026-04-08 10:59:28 +0530

    second commit

---
+++ src/DsaQuestions/LongestCommon.java
@@ -0,0 +1,25 @@
+class LCS {
+    static int lcs(String X, String Y, int m, int n) {
+        int dp[][] = new int[m + 1][n + 1];
+
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if(X.charAt(i - 1) == Y.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String X = "AGGTAB";
+        String Y = "GXTXAYB";
+
+        System.out.println("LCS Length: " + lcs(X, Y, X.length(), Y.length()));
+    }
+}
