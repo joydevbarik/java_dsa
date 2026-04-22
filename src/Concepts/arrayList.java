347becb3325d94245228be05475847fe187855b3
Joydev Barik <barikjoydev263@gmail.com>
2026-04-22 23:47:52 +0530

    arrayList

---
+++ src/Concepts/arrayList.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class arrayList {
+    public static String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(currentRow).append(c);
+
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = sc.nextInt();
+
+        System.out.println(convert(s, n));
+    }
+}
